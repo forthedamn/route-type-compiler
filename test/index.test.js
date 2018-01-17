4eae6a2e80b5e12cb800112bd569495857ea973f
zhongguoxin <zhongguoxin@meituan.com>
2018-01-17 15:55:11 +0800

    chore: remove js

--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-require('jest');
-const shelljs = require('shelljs');
-describe('exec', () => {
-    it('file', () => {
-        shelljs.exec('node /Users/forthedamn/npm_node_modules/route-type-compiler/index.js -i ./test/fixture/route -o ./test/fixture/route -m test');
-    });
-});
