4d3709942e599fe431a3ca75ad8aa6b0ddc8415c
zhongguoxin <zhongguoxin@meituan.com>
2018-07-02 19:18:03 +0800

    fix(path): ../server/route

--- lib/type_template.ts
+++ lib/type_template.ts
@@ -15,7 +15,8 @@ export default function(filePathList: Array<string>, input: string) {
    */
   const importPool = [];
   function importGen(instanceName: string, routeFilePath: string) {
-    routeFilePath = routeFilePath.replace(/\.\//, '../route/');
+    // TODO: 动态配置
+    routeFilePath = routeFilePath.replace(/\.\//, '../server/route/');
     importPool.push(`import ${upperHelper(instanceName)} from '${routeFilePath.replace(/.ts/, '')}';`);
   }
   /**
